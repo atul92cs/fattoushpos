c8ecb7074b82b4152c23227c0faf3da40e1a31f3
hthofficial <hthofficial2017@gmail.com>
2018-01-27 14:24:26 +0530

    imoroving pos

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 var express = require('express');
 var bodyparer=require('body-parser');
 var mysql=require('mysql');
+
+var db=mysql.createConnection( {host:'trial.cpaoqpenaokn.us-east-2.rds.amazonaws.com',
+	  user:'admin',
+	  password:'savita92',
+database:'fattoush'});
+
+db.connect((error)=>{
+    if(error)throw error;
+	
+	console.log('Database connected');
+});
+
+var port=process.env.PORT||8080;
+
+var app = express();
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.post('/',function(req,res){
+	res.send('App works');
+});
+
+app.listen(port,function(){
+      console.log('app started on port 8080');
+  });
